7380c3bb9f9ac54daca7b42dd99533777799a26e
Chinmayan Pradeep <chinmayanks@gmail.com>
2025-01-03 18:16:03 +0400

    analytics+speed inline components

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
