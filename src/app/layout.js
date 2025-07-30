63404b90969e6525c3c81acb9c6136ab7bd5b428
Chin <chinmayanks@gmail.com>
2025-07-30 18:56:47 -0400

    text change

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
         {children}
         {/* Footer */}
         <footer className="bg-white dark:bg-white text-left text-[11px] sm:text-[13px] md:text-[14px] text-gray-600 w-full px-16 sm:px-40 md:px-64 py-4 font-geist">
-          This page documents my old Instagram feed&nbsp;— an unpolished archive of memories and other ephemera from when I was a teenager. If you&apos;re curious about what I&apos;m up to now or you just want to say hello, visit&nbsp;
+          This page documents my old Instagram feed&nbsp;— an archive of memories and other ephemera from when I was a teenager. If you&apos;re curious about what I&apos;m up to now or you just want to say hello, visit&nbsp;
           <a href="https://chin.bio" className="underline hover:text-gray-800" target="_blank" rel="noopener noreferrer">chin.bio</a>!
         </footer>
         <Analytics />
